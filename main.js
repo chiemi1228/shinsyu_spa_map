e702eef87a97c0357ee9d1a7404a6cf771ffc720
chiemi1228 <sucreunpeu@yahoo.co.jp>
2019-07-19 16:00:06 +0900

    右側に、"天川荘"が表示された

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 $(function() {
-    $('.map_amanogawasou').click(function() {
-      $('.right_info').append('add_html/amanogawasou.html');
+   $(".map_amanogawasou").click(function() {
+        $(".right_info").text("天川荘");
     });
-  
-    $('.index2').click(function() {
-      window.location = './index2.html';
-    });
-  
-    $('.index3').click(function() {
-      window.location = './index3.html';
-    });
-  });
+});
