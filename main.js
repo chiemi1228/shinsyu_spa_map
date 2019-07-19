e1569ba82704238be730980737fd7c54581ef697
chiemi1228 <sucreunpeu@yahoo.co.jp>
2019-07-19 14:48:51 +0900

    jsがうまく行かない。ガラッと帰る

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.map_amanogawasou').click(function() {
+      $('.right_info').append('add_html/amanogawasou.html');
+    });
+  
+    $('.index2').click(function() {
+      window.location = './index2.html';
+    });
+  
+    $('.index3').click(function() {
+      window.location = './index3.html';
+    });
+  });
