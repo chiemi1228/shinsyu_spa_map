f7cefc5942dc4aec948e4ec82426c04aca1ae0f1
chiemi1228 <sucreunpeu@yahoo.co.jp>
2019-07-19 21:54:42 +0900

    天川荘だけできた

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(function() {
-   $(".map_amanogawasou").click(function() {
-        $(".right_info").text("天川荘");
+   $(".map_amanogawasou").on("click", function() {
+        $(".right_info").html('<div class="amanogawasou_section"><div class="right-info_text"><h4>木漏れ日の下で、足湯に入って行きませんか？</h4></div><img src="img/amanogawasou/amanogawasou-2.jpg"><img src="img/amanogawasou/amanogawasou.jpeg"><div class="right-info_text"><p>もともとは休憩場所だった東屋を改装し、足湯として新たにオープン。囲いの板塀や腰掛け板にはヒノキを、湯口には小川の石をあしらった清涼感ある造りで、一度に16人ほどが並んで利用できます。</p></div></div>', function() {
+
+        });
     });
 });
