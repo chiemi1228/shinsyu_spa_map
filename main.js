fe314d5ea17a7cf8a1a5467fa25c804f88a9316a
chiemi1228 <sucreunpeu@yahoo.co.jp>
2019-07-20 23:53:36 +0900

    画像が切り替わるようになった

--- main.js
+++ main.js
@@ -2,15 +2,7 @@ $(function() {
    $(".map_amanogawasou").on("click", function() {
         $(".right-info_section").html('<div class="right-info_title"><h4>天川荘</h4></div><img src="img/amanogawasou/amanogawasou-2.jpg"><img src="img/amanogawasou/amanogawasou.jpeg"><div class="right-info_text"><p>もともとは休憩場所だった東屋を改装し、足湯として新たにオープン。囲いの板塀や腰掛け板にはヒノキを、湯口には小川の石をあしらった清涼感ある造りで、一度に16人ほどが並んで利用できます。</p></div>');
     });
-});
-$(function() {
     $(".masaki_ryokan").on("click", function() {
         $(".right-info_section").html('<div class="right-info_title"><h4>政喜旅館</h4></div><img src="img/masaki_ryokan/masaki_ryokan2.jpeg"><img src="img/masaki_ryokan/masaki_ryokan1.jpeg"><div class="right-info_text"><p>ここ10年は川底から自然に湧き出す奇跡の名湯を、食前酒代わりの一杯や料理にも活用。温泉しゃぶしゃぶは作さくしゅう州牛に次いで、月間の出荷頭数が20頭に限られた千屋牛も堪能できるように。</p></div>');
-    });
-});
-
-$(function() {
-    $(".masaki_ryokan").on("click", function() {
-        $(".right_info").html('<div class="right-info_section"><div class="right-info_title"><h4>政喜旅館</h4></div><img src="img/amanogawasou/amanogawasou-2.jpg"><img src="img/amanogawasou/amanogawasou.jpeg"><div class="right-info_text"><p>政喜旅館から徒歩1分の所にある六番湯の目洗いの湯です。昔から多くの人々が眼病を癒しに来たと言われています。また肌にも良いと言われていて、美人の湯とも呼ばれます。</p></div></div>');
     });
 });
