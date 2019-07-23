28e496877298bfdb21c7581c37189f2db21f79fc
chiemi1228 <sucreunpeu@yahoo.co.jp>
2019-07-23 23:05:21 +0900

    イメージマップ完成
    イメージマップ完成。
    タッチして表示まで完了。

--- main.js
+++ main.js
@@ -1,27 +1,92 @@
-
 $(function() {
-
-
     $(".map_amanogawasou").on("click", function() {
-        $('.left_map_touch-point').html('<div class="touch_map">■</div>').hide().fadeIn(400);
+        $('.left_map_touch-point').html('<div class="touch_map">★</div>');
         $('.touch_map').css({
-            'position':'absolute','left':'525px','top':'220px'
-    });
-        $(".right-info_section").html('<div class="right-info_title"><h4>天川荘</h4></div><img src="img/amanogawasou/amanogawasou-2.jpg"><img src="img/amanogawasou/amanogawasou.jpeg"><div class="right-info_text"><p>もともとは休憩場所だった東屋を改装し、足湯として新たにオープン。囲いの板塀や腰掛け板にはヒノキを、湯口には小川の石をあしらった清涼感ある造りで、一度に16人ほどが並んで利用できます。</p></div>').hide().fadeIn(400);
+            'position':'absolute','left':'550px','top':'280px'
+        }); 
+        $(".right-info_section").html('<div class="right-info_title"><h4>光が降り注ぐ美人湯は良縁成就にご利益アリ</h4></div><img src="img/amanogawa/amanogawa_spa.jpeg"><img src="img/amanogawa/amanogawa_foot-spa.jpeg"><div class="right-info_text"><p>ガラス張りの天井から太陽の光が優しく降り注ぐ浴室は開放感いっぱい！温泉のミストが吹出すミストサウナや、自然の緑に囲まれた露天風呂もあって、つい長湯しちゃいそうに。ガラス張りの天井から太陽の光が優しく降り注ぐ浴室は開放感いっぱい！温泉のミストが吹出すミストサウナや、自然の緑に囲まれた露天風呂もあって、つい長湯しちゃいそうに。</p></div>').hide().fadeIn(800);
+        $('p').css('max-width','600px');
     });
 
 
 
 
     $(".masaki_ryokan").on("click", function() {
-        $('.left_map_touch-point').html('<div class="touch_map">■</div>').hide().fadeIn(400);
+        $('.left_map_touch-point').html('<div class="touch_map">★</div>');
         $('.touch_map').css({
-            'position':'absolute','left':'405px','top':'390px'
+            'position':'absolute','left':'430px','top':'460px'
+        });
+        $('p').css('max-width','455px');
+        $(".right-info_section").html('<div class="right-info_title"><h4>舌を躍らせる、絶世の美肉千屋牛</h4></div><img src="img/masaki/masaki_beef.jpeg"><img src="img/masaki/masaki_spa.jpeg"><div class="right-info_text"><p>ここ10年は川底から自然に湧き出す奇跡の名湯を、食前酒代わりの一杯や料理にも活用。温泉しゃぶしゃぶは作州牛に次いで、月間の出荷頭数が20頭に限られた千屋牛も堪能できるように。ここ10年は川底から自然に湧き出す奇跡の名湯を、食前酒代わりの一杯や料理にも活用。温泉しゃぶしゃぶは作州牛に次いで、月間の出荷頭数が20頭に限られた千屋牛も堪能できるように。</p></div>').hide().fadeIn(800);
+        $('p').css('max-width','620px');
     });
-        $(".right-info_section").html('<div class="right-info_fade-in"><div class="right-info_title"><h4>政喜旅館</h4></div><img src="img/masaki_ryokan/masaki_ryokan2.jpeg"><img src="img/masaki_ryokan/masaki_ryokan1.jpeg"><div class="right-info_text"><p>ここ10年は川底から自然に湧き出す奇跡の名湯を、食前酒代わりの一杯や料理にも活用。温泉しゃぶしゃぶは作さくしゅう州牛に次いで、月間の出荷頭数が20頭に限られた千屋牛も堪能できるように。</p></div></div>').hide().fadeIn(400);
+
+
+
+    
+    $(".suminoyu").on("click", function() {
+        $('.left_map_touch-point').html('<div class="touch_map">★</div>');
+        $('.touch_map').css({
+            'position':'absolute','left':'310px','top':'490px'
+        });
+        $('p').css('max-width','450px');
+        $(".right-info_section").html('<div class="right-info_title"><h4>じゅわっとふわっと食感の極上フレンチトースト</h4></div><img src="img/suminoyu/suminoyu_store-inner.jpeg"><img src="img/suminoyu/suminoyu_french.jpeg"><div class="right-info_text"><p>古い御宿を改築したカフェ「御宿　炭乃湯」。店内はレトロさを残しつつもシックでお洒落。ところどころに古いミシンや箪笥などの道具が置かれ、モダンな雰囲気の中にも風情と温かみが感じられます。専用のパンで作るフレンチトーストは、優しい食感でひと口ごとににんまり。地元産いちごや自家製いちごバター、アイスクリームを添えつつ、あっという間にごちそうさま♪珈琲や自家製レモネードの他、ポルトガルの料理など。ゆっくりと時間を過ごせるカフェです。</p></div>').hide().fadeIn(800);
+        $('p').css('max-width','620px');
+    });
+    
+
+
+    $(".komatuya").on("click", function() {
+        $('.left_map_touch-point').html('<div class="touch_map">★</div>');
+        $('.touch_map').css({
+            'position':'absolute','left':'240px','top':'490px'
+    });
+        $(".right-info_section").html('<div class="right-info_title"><h4>浴衣に合わせてバッグや小物を選んでみては？</h4></div><div class="img_box"><img src="img/komatuya/komatuya_store-inner.jpeg"><img src="img/komatuya/komatuya_bag2.jpeg"></div><div class="right-info_text"><p>風情あふれる温泉地･草津の中心にあるユニークなバッグ専門店。素材、柄、デザインを重視、職人技が光る世界中のバッグや雑貨をセレクトしています。日本の風情と異国情緒のコラボレーション、魅力あふれる商品をぜひ手にとってみてくださいね。</p></div>').hide().fadeIn(800);
+        $('p').css('max-width','570px');
+    });
+
+
+
+    $(".yorozuya").on("click", function() {
+        $('.left_map_touch-point').html('<div class="touch_map">★</div>');
+        $('.touch_map').css({
+            'position':'absolute','left':'410px','top':'540px'
+    });
+        $(".right-info_section").html('<div class="right-info_title"><h4>老舗和菓子店のこだわりが詰まったバウムクーヘン。</h4></div><img src="img/yorozuya/yorozuya_barm.jpeg"><img src="img/yorozuya/yorozuya_store_inner.jpeg"><div class="right-info_text"><p>伝統の味を生み出してきた歴史ある和菓子店。看板商品「よろず屋バウム」は、まるでカステラのような見た目のバウムクーヘン。こだわりの材料と匠の技で作り出された懐かしい味わいが人気です。美しく層になった生地とちりばめた粗目糖、口に含むとしっとりとした生地に粗目糖の食感がアクセントに。景色を楽しみながら、ゆっくりとお過ごしいただけます。</p></div>').hide().fadeIn(800);
+        $('p').css('max-width','630px');
+    });
+
+
+    $(".sineikan").on("click", function() {
+        $('.left_map_touch-point').html('<div class="touch_map">★</div>');
+        $('.touch_map').css({
+            'position':'absolute','left':'290px','top':'580px'
+    });
+        $(".right-info_section").html('<div class="right-info_title"><h4>可愛いお土産はここで手に入れよう</h4></div><img src="img/sinekan/sinekan_store-inner.jpeg"><img src="img/sinekan/sineikan_mamezara.jpeg"><div class="right-info_text"><p>現在、名物ともなっている「有馬サイダーてっぽう水」や「カメ印美肌石鹸」などを販売しています。また、フェイスマスクや入浴剤など、ちょっとしたお土産にぴったりのものが多く揃いますので、ぜひ立ち寄ってみてください。きっと、思い出に残るお土産が見つかるはずですよ！</p></div>').hide().fadeIn(800);
+        $('p').css('max-width','570px');
+    });
+
+
+
+
+    $(".kinki").on("click", function() {
+        $('.left_map_touch-point').html('<div class="touch_map">★</div>');
+        $('.touch_map').css({
+            'position':'absolute','left':'430px','top':'590px'
+    });
+        $(".right-info_section").html('<div class="right-info_title"><h4>飲みやすい地ビールと共に気軽に楽しむカニグルメ</h4></div><img src="img/kinnki/kinnki_pasta2.jpeg"><img src="img/kinnki/kinnki_beer.jpeg"><div class="right-info_text"><p>具材にカニの身を贅沢に使う変わりぺペロンチーノ。カニの甘みと海の香り、唐辛子のスパイシーさが後を引く美味しさ。4種の地ビールは生でもお土産にも（500円～）。酵母が生きた地ビールは美容効果にいいってウワサも！</p></div>').hide().fadeIn(800);
+        $('p').css('max-width','550px');
     });
 
 });
+
+
+
+// 小松屋旅館    個性的で温かいバッグや小物たちが勢ぞろい！浴衣に合わせて選べば「湯めぐり」のお供に。　　　風情あふれる温泉地･草津の中心にあるユニークなバッグ専門店。素材、柄、デザインを重視、職人技が光る世界中のバッグや雑貨をセレクトしています。日本の風情と異国情緒のコラボレーション、魅力あふれる商品をぜひ手にとってみてくださいね。 
+// 御宿　炭乃湯　　　じゅわっとふわっと食感の極上フレンチトースト　古い御宿を改築したカフェ「御宿　炭乃湯」。店内はレトロさを残しつつもシックでお洒落。ところどころに古いミシンや箪笥などの道具が置かれ、モダンな雰囲気の中にも風情と温かみが感じられます。専用のパンで作るフレンチトーストは、優しい食感でひと口ごとににんまり。地元産いちごや自家製いちごバター、アイスクリームを添えつつ、あっという間にごちそうさま♪珈琲や自家製レモネードの他、ポルトガルの料理など。ゆっくりと時間を過ごせるカフェです。
+// よろず屋旅館　　　老舗和菓子店のこだわりが詰まったバウムクーヘン。　　伝統の味を生み出してきた歴史ある和菓子店。看板商品「よろず屋バウム」は、まるでカステラのような見た目のバウムクーヘン。こだわりの材料と匠の技で作り出された懐かしい味わいが人気です。美しく層になった生地とちりばめた粗目糖、口に含むとしっとりとした生地に粗目糖の食感がアクセントに。景色を楽しみながら、ゆっくりとお過ごしいただけます。
+// 金喜ホテル　　　　飲みやすい地ビールと共に気軽に楽しむカニグルメ。　　具材にカニの身を贅沢に使う変わりぺペロンチーノ。カニの甘みと海の香り、唐辛子のスパイシーさが後を引く美味しさ。4種の地ビールは生でもお土産にも（500円～）。酵母が生きた地ビールは美容効果にいいってウワサも！
+// 新栄館　　　　　可愛いお土産はここで手に入れよう　　　　　　　　　現在、名物ともなっている「有馬サイダーてっぽう水」や「カメ印美肌石鹸」などを販売しています。また、フェイスマスクや入浴剤など、ちょっとしたお土産にぴったりのものが多く揃いますので、ぜひ立ち寄ってみてください。きっと、思い出に残るお土産が見つかるはずですよ！
 
 
 
